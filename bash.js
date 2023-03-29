3732513236754e5bf9ab19d7f6f523edfb448d76
bernasanchez <bernardoluisanchez25@gmail.com>
2023-03-29 17:20:03 -0300

    add nodeCLI

---
+++ bash.js
@@ -0,0 +1,53 @@
+// Un prompt como output
+///console.log(process, Object.keys(process))
+const { isUtf8 } = require("buffer");
+let fs = require("fs");
+let comandos = require("./command");
+process.stdout.write("prompt > ");
+// El evento STDIN 'data' se dispara cuando el usuario escribe una línea
+process.stdin.on("data", function (data) {
+  let cmd = data.toString().trim(); // Remueve la nueva línea
+  let palabras = data.toString().split(" ");
+
+  if (cmd == "pwd") comandos.done(comandos.pwd());
+
+  if (cmd == "date") comandos.done(comandos.date());
+
+  if (cmd == "ls") comandos.ls();
+
+  if (palabras[0] == "cat" || cmd == "cat") {
+    if (palabras[1]) {
+      palabras[1] = palabras[1].substring(0, palabras[1].length - 1);
+      comandos.done(comandos.cat(palabras[1]));
+    }
+  }
+  if (palabras[0] == "head" || cmd == "head") {
+    if (palabras[2]) {
+      comandos.head(palabras[1], palabras[2]);
+    } else if (palabras[1]) {
+      comandos.head(palabras[1]);
+    }
+  }
+  if (palabras[0] == "tail" || cmd == "tail") {
+    if (palabras[2]) {
+      comandos.tail(palabras[1], palabras[2]);
+    } else if (palabras[1]) {
+      comandos.tail(palabras[1]);
+    }
+  }
+  if (palabras[0] == "wc" || cmd == "wc") {
+    palabras[1] = palabras[1].substring(0, palabras[1].length - 1);
+    comandos.wc(palabras[1]);
+  }
+  if (palabras.shift() == "echo") comandos.echo(palabras.join(" "));
+});
+
+/*
+const startTime = new Date();
+
+setTimeout(function () {
+  const endTime = new Date();
+  console.log('Time elapsed: ', endTime - startTime, 'ms');
+}, 500);
+
+while (new Date() - startTime < 1000) {};*/
---
+++ command.js
@@ -0,0 +1,61 @@
+nodlet fs = require("fs");
+module.exports = {
+  pwd: function () {
+    // Código pwd
+    return process.argv[1];
+  },
+  date: function () {
+    let date = new Date();
+    return `${date}`;
+  },
+  ls: function () {
+    fs.readdir(".", function (err, files) {
+      if (err) throw err;
+      let arr = [];
+      files.forEach(function (file) {
+        arr.push(file.toString());
+      });
+      let str = arr.join("\n");
+      return str;
+    });
+  },
+  echo: (string) => {
+    return string;
+  },
+  cat: (txt) => {
+    fs.readFile(txt, "utf8", function (err, data) {
+      if (err) console.log("nombre de archivo inexistente");
+      return data;
+    });
+  },
+  head: (txt, n = 5) => {
+    fs.readFile(txt, "utf8", function (err, data) {
+      if (err) console.log("nombre de archivo inexistente");
+      let lineas = data.split("\n");
+      for (let i = 0; i < n; i++) {
+        if (i < lineas.length) return lineas[i];
+      }
+    });
+  },
+  tail: (txt, n = 5) => {
+    fs.readFile(txt, "utf8", function (err, data) {
+      let lineas = data.split("\n");
+      if (err) console.log("nombre de archivo inexistente");
+      for (let i = 0; i < n; i++) {
+        if (i < lineas.length) return lineas[lineas.length - 1 - i];
+      }
+    });
+  },
+  wc: (txt, n = 5) => {
+    fs.readFile(txt, "utf8", function (err, data) {
+      if (err) console.log(err);
+      let lineas = data.split("\n");
+      console.log(lineas.length);
+      if (err) return "nombre de archivo inexistente";
+    });
+  },
+  done: (output) => {
+    process.stdout.write(output);
+    process.stdout.write("\nprompt > ");
+  },
+};
